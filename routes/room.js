f92b7abbc4f4ce5db246198a63f0047945ab9f8c
remihhardy <99190560+remihhardy@users.noreply.github.com>
2022-06-01 10:42:38 +0200

    we can add and get room : adding a room need a token; and send back room id; need to create bet model in order to finalize the get

---
+++ routes/room.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const roomRoute = express.Router()
+const authRoute = require("../middlewares/auth")
+
+const roomController = require("../controller/roomController")
+
+// CREATE
+roomRoute.post("/room",authRoute, roomController.addRoom)
+
+
+roomRoute.get("/room/:id", roomController.getRoom)
+
+roomRoute.get("/room" , roomController.getRoom)
+//// TODO :
+// roomRoute.put("/room/:id", roomController.updateRoom)
+//
+// // DELETE
+// roomRoute.delete("/room", roomController.deleteRoom)
+
+module.exports = roomRoute
