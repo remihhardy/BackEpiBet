75239ad009851afcb62f98f51d41c0d4e108733b
remihhardy <99190560+remihhardy@users.noreply.github.com>
2022-06-02 13:16:51 +0200

    test heroku

--- app.js
+++ app.js
@@ -30,7 +30,9 @@ mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('Connected to DB'))
   .catch(console.error)
 
-
+app.listen(process.env.PORT || 3001, () => {
+  console.log('Server UP')
+})
 app.use('/', (req, res) => {
   return res.status(200).json({ result: 'server up !' })
 })
